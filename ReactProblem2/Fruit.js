da5c20fbc73aa0e70031363168f4fb342a5c4696
Akash Singhal <akash.singhal67@yahoo.com>
2019-09-09 14:06:44 +0530

    problem statement 2

---
+++ ReactProblem2/Fruit.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import TypeOfFruit from "./TypeOfFruit"
+
+export default class Order extends Component {
+  
+  render() {
+    return (
+        <TypeOfFruit />);
+     
+  }
+}
---
+++ ReactProblem2/TypeOfFoodComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Fruit from "./Fruit" 
+
+
+export default class TypeOfFoodComponent extends Component {
+ 
+
+  render() {
+    return (
+      <Fruit />
+    );
+  }
+}
